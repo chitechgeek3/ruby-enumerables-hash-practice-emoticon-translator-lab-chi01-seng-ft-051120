bb805301e45babc9501ea1747f6ec0f0fa1c2139
Rob Lawrence <iamrobnyce@gmail.com>
2020-05-11 07:37:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
 # require modules here
 
-def load_library
+  def load_library(filepath)
   # code goes here
+  
+  emoji_data = YAML.load_file(filepath)
+#  emoji_data.transform_keys!(&:to_sym)
+  emoji_hash = {}
+  emoji_data.each do |emotion_txt, emotion_sym|
+    if emoji_hash[emotion_txt] == nil
+      emoji_hash[emotion_txt] = {:english => emotion_sym[0], :japanese => emotion_sym[1]}
+    end
+  end
+#  binding.pry
+  emoji_hash
+end
+
 end
 
 def get_japanese_emoticon
